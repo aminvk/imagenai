de46fd2d1b9084d979fe4f215f593939d98f5f82
amin vakili <aminvakili@amins-MacBook-Pro.local>
2024-05-31 13:37:38 +0200

    newupdate

--- components/shared/CustomField.tsx
+++ components/shared/CustomField.tsx
@@ -1,43 +1,9 @@
-import React from "react";
-import { Control } from "react-hook-form";
-import { z } from "zod";
+import React from 'react'
 
-import {
-  FormField,
-  FormItem,
-  FormControl,
-  FormMessage,
-  FormLabel,
-} from "../ui/form";
-
-import { formSchema } from "./TransformationForm";
-
-type CustomFieldProps = {
-  control: Control<z.infer<typeof formSchema>> | undefined;
-  render: (props: { field: any }) => React.ReactNode;
-  name: keyof z.infer<typeof formSchema>;
-  formLabel?: string;
-  className?: string;
-};
-
-export const CustomField = ({
-  control,
-  render,
-  name,
-  formLabel,
-  className,
-}: CustomFieldProps) => {
+const CustomField = () => {
   return (
-    <FormField
-      control={control}
-      name={name}
-      render={({ field }) => (
-        <FormItem className={className}>
-          {formLabel && <FormLabel>{formLabel}</FormLabel>}
-          <FormControl>{render({ field })}</FormControl>
-          <FormMessage />
-        </FormItem>
-      )}
-    />
-  );
-};
+    <div>CustomField</div>
+  )
+}
+
+export default CustomField
